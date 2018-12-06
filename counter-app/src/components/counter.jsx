c058b353903cbe40ac3f094be7cca2240ea64281
kedards25 <kedards25@gmail.com>
2018-12-06 12:50:07 +0530

    conditional rendering going on

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,23 +1,39 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    data: ["data1","data2","data3"]
   };
 
   styles = {
     fontStyle: "italic"
   };
+
+  renderData() {
+    if (this.state.data.length === 0) return <h4 className="alert-danger w-25">No Data</h4>;
+    return <ul>
+      {this.state.data.map(data => <li key={data}>{data}</li>)}
+    </ul>;
+  }
   render() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+
+    //let classes = this.newBadgeMethod();
     return (
       <div>
-        <span style={this.styles} className={classes}>
+        <span style={this.styles} className={this.newBadgeMethod()}>
           {this.formatCount()}
         </span>
-        <button class="btn btn-secondary">Add 1</button>
+        {/* {this.state.data.map(data => <li key={data}>{data}</li>)} */}
+          {this.renderData()}
+        <button className="btn btn-secondary">Add 1</button>
       </div>
     );
+  }
+
+  newBadgeMethod() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
